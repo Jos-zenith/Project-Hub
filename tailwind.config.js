1a1d37041010402860e6e58c5ab227940d9b3db1
Zenith Joshua <zenithjoshua.27it@licet.ac.in>
2026-05-06 00:30:29 +0530

    Add repo config files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#800020',
+        turmeric: '#FFD700',
+        emerald: '#228B22',
+        charcoal: '#333333',
+        offwhite: '#F5F5F5'
+      },
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'],
+        body: ['Lora', 'serif'],
+        fun: ['Baloo 2', 'cursive']
+      }
+    }
+  },
+  plugins: []
+}
